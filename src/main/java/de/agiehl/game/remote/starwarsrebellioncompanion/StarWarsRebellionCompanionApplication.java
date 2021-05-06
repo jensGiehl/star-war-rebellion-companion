4ee7cb6bdd3c0265199da60f6be4981c3a2090bd
Jens Giehl <mobil.jgiehl@gmail.com>
2021-05-06 21:26:26 +0200

    Init commit

---
+++ src/main/java/de/agiehl/game/remote/starwarsrebellioncompanion/StarWarsRebellionCompanionApplication.java
@@ -0,0 +1,13 @@
+package de.agiehl.game.remote.starwarsrebellioncompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsRebellionCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsRebellionCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/agiehl/game/remote/starwarsrebellioncompanion/StarWarsRebellionCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.agiehl.game.remote.starwarsrebellioncompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsRebellionCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
